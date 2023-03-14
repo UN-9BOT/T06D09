37fca2972272b4abfb2ef2a151dcf1cbd4989686
Yajaira Hercules <un.9bot@gmail.com>
2023-03-14 11:03:48 +0300

    develop / ex 1-3

--- src/key9part1.c
+++ src/key9part1.c
@@ -5,53 +5,57 @@
 -------------------------------------*/
 
 #include <stdio.h>
+#define NMAX 10
 
-void input (int *buffer, int *length);
+void input (int *buffer, int *length, int *);
 void output (int *buffer, int length);
 int sum_numbers(int *buffer, int length);
-int find_numbers(int* buffer, int length, int number, int* numbers)
+void find_numbers(int* buffer, int length, int number, int* numbers);
 
-/*------------------------------------
-	Функция получает массив данных 
-	через stdin.
-	Выдает в stdout особую сумму
-	и сформированный массив 
-	специальных элементов
-	(выбранных с помощью найденной суммы):
-	это и будет частью ключа
--------------------------------------*/
-int main()
-{
-    
+int main() {
+    int n, buffer[NMAX], numbers[NMAX], number;
+    int flag = 0;
+
+    input(buffer, &n, &flag);
+    if (flag != 1) {
+        printf("n/a");
+    } else {
+        number = sum_numbers(buffer, n);
+        find_numbers(buffer, n, number, numbers);
+        output(numbers, n);
+    }
+    return (0);
 }
 
-/*------------------------------------
-	Функция должна находить
-	сумму четных элементов 
-	с 0-й позиции.
--------------------------------------*/
-int sum_numbers(int *buffer, int length)
-{
+int sum_numbers(int *buffer, int length) {
 	int sum = 0;
 	
 	for (int i = 1; i < length; i++)
 	{
-		if (i % 2 != 0)
+		if (i % 2 == 0)
 		{
-			sum = sum + buffer[i];
+			sum += buffer[i];
 		}
 	}
 	
 	return sum;
 }
 
-/*------------------------------------
-	Функция должна находить
-	все элементы, на которые нацело
-	делится переданное число и
-	записывает их в выходной массив.
--------------------------------------*/
-int find_numbers(int* buffer, int length, int number, int* numbers)
-{
+void find_numbers(int* buffer, int length, int number, int* numbers) {
+    for (int i = 0, k = 0; i < length; i++) {
+        if (*(buffer + i) % number == 0) { *(numbers + k++) = *(buffer + i); }
+    }
+}
 
+void input(int *data, int *n, int *flag) {
+    int k;
+    if (scanf("%d.%d", n, &k) != 1) {
+        *flag = 1;
+    } else {
+        for (int i = 0; i < NMAX; i++) {
+            if (scanf("%d.%d", (data + i), &k) != 1) {
+                *flag = 1;
+            }
+        }
+    }
 }
